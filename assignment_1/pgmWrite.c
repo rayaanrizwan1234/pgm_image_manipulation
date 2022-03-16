7a1907d8080b9fc32a2379d29a9b56e3f5ffa7bf
Rayaan Rizwan <sc21rr@leeds.ac.uk>
2022-03-16 14:45:27 +0000

    Seperated Write function and validations

---
+++ assignment_1/pgmWrite.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "pgmEchoStructs.h"
+#include "pgmDef.h"
+#include "pgmWrite.h"
+#include "pgmValidationWrite.h"
+int write (char *input, File *file1) {
+	FILE *outputFile = fopen(input, "w");
+
+	  nImageBytes = file1->width * file1->height * sizeof(unsigned char);
+	 /* check whether file opening worked     */
+	 checkFile(outputFile, file1);
+	 /* write magic number, size & gray value */
+	//  size_t nBytesWritten  = fprintf(outputFile, "P2\n%d %d\n%d\n", file1->width, file1->height, file1->maxGray);
+	 size_t nBytesWritten = fwrite((unsigned char *)(file1->imageData), sizeof nImageBytes, outputFile);
+	 /* check that dimensions wrote correctly */
+		checknBytesWritten(nBytesWritten, file1);
+
+			 /* pointer for efficient write code      */
+			 for (unsigned char *nextGrayValue = file1->imageData; nextGrayValue < file1->imageData + nImageBytes; nextGrayValue++)
+							 { /* per gray value */
+			 /* get next char's column        */
+			 int nextCol = (nextGrayValue - file1->imageData + 1) % file1->width;
+
+			 /* write the entry & whitespace  */
+			 nBytesWritten = fprintf(outputFile, "%d%c", *nextGrayValue, (nextCol ? ' ' : '\n') );
+
+			 checknBytesWritten(nBytesWritten, file1);
+			 } /* per gray value */
+
+	 /* at this point, we are done and can exit with a success code */
+	 return EXIT_NO_ERRORS;
+}
